d367c7aa2235da4befac5cc98556dfa077f671eb
Kevin Phair <kevin.phair@gmail.com>
2015-06-26 10:47:45 +0100

    Used new Person class in greeting

--- HelloWorld/src/HelloWorldApp.java
+++ HelloWorld/src/HelloWorldApp.java
@@ -2,19 +2,25 @@
 public class HelloWorldApp {
 
 	public static void main(String[] args) {
-		// Set up a String for later
-		String testString;
+		
+		// Set up an identity for later
+		Person p = new Person();
 		
 		// Check for any supplied arguments
 		if (args.length > 0) {
 			// If there is a user-supplied string, use that
-			testString = args[0];
+			p.setName (args[0]);
 		} else {
-			// Otherwise, use the following default
-			testString = "Professor.\nWould you like to play a game?";
+			// Otherwise, use the following default if the
+			// class has no default name
+			if (p.getName()==null) {
+				System.out.println("* No name set in class, setting a default");
+				p.setName("Professor");
+			}
 		}
 		
-		System.out.println("Hello, "+testString);
+		System.out.println("Hello, "+p.getName());
+		System.out.println("Would you like play a game?");
 	}
 
 }
