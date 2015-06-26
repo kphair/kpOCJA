d17818dffefc8ef090aebb780a86b3f7ba9e2229
Kevin Phair <kevin.phair@gmail.com>
2015-06-26 09:50:28 +0100

    Tidy up and comments

--- HelloWorld/src/HelloWorldApp.java
+++ HelloWorld/src/HelloWorldApp.java
@@ -2,14 +2,18 @@
 public class HelloWorldApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Set up a String for later
 		String testString;
 		
+		// Check for any supplied arguments
 		if (args.length > 0) {
+			// If there is a user-supplied string, use that
 			testString = args[0];
 		} else {
+			// Otherwise, use the following default
 			testString = "Professor.\nWould you like to play a game?";
 		}
+		
 		System.out.println("Hello, "+testString);
 	}
 
