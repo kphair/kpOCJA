dc2ac4274c6027e4d1b08243fe4010f678d8b45b
Kevin Phair <kevin.phair@gmail.com>
2015-06-26 10:25:31 +0100

    Created a Person class

---
+++ HelloWorld/src/Person.java
@@ -0,0 +1,32 @@
+// Set up a Person class
+public class Person {
+
+	int age;
+	String name;
+	char gender;
+	
+	// age getters and setters
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	// name getters and setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// gender getters and setters
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+}
