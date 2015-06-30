0936b978e1c558f5abd288b7b2595fd6a58fb1cb
Kevin Phair <kevin.phair@gmail.com>
2015-06-30 23:12:31 +0100

    Added some comments by way of explanation for FloatTest

--- OCJA15/src/com/kevinphair/ocja/experiments/FloatTest.java
+++ OCJA15/src/com/kevinphair/ocja/experiments/FloatTest.java
@@ -1,7 +1,19 @@
 /**
+ * A test app to store ints as floats and compare the fidelity
  * 
+ * As suspected, once the int goes over a value of 2^24, the number
+ * is scaled down fit within a float's storage requirements.
+ * 
+ * Converting back to integer results in most of the floats no longer
+ * matching their original ints
  */
 package com.kevinphair.ocja.experiments;
+
+/**
+ * @author Kevin Phair
+ * @version 1.0
+ * @see https://en.wikipedia.org/wiki/IEEE_754-1985
+ */
 
 public class FloatTest {
 
