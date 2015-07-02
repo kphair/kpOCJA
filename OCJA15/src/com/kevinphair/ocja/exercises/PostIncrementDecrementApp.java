cfd22093a8bf24ca75d836bc34432f15fd9ae115
Kevin Phair <kevin.phair@gmail.com>
2015-07-02 09:39:32 +0100

    Added age range checks to increment/decrement exercise

--- OCJA15/src/com/kevinphair/ocja/exercises/PostIncrementDecrementApp.java
+++ OCJA15/src/com/kevinphair/ocja/exercises/PostIncrementDecrementApp.java
@@ -1,3 +1,8 @@
+/**
+ * Exercises for the Java programming course 
+ * @author Kevin Phair
+ *
+ */
 package com.kevinphair.ocja.exercises;
 
 /*
@@ -5,21 +10,37 @@ package com.kevinphair.ocja.exercises;
  */
 public class PostIncrementDecrementApp {
 	
+	static final int MIN_AGE = 18;
+	static final int MAX_AGE = 199;
+
 	public static void main(String[] args) {
 		
-		int age = 20;
+		int age = 200;
 		
-		// Show the initial value of age
-		System.out.println("Initial value of age: " + age);
+		// Show the initial value of age only if it is 18 or higher and less than 200
+		if (ageCheck(age)) {
+			System.out.println("Initial value of age: " + age);
+		} else {
+			System.out.println("Welcome, Lo-Pan");
+		}
 		
 		// Increment and show the new value of age
 		age++;
-		System.out.println("Incremented age: " + age);
+		if (ageCheck(age)) {
+			System.out.println("Incremented age: " + age);
+		}
 		
 		// Decrement and show the new value of age
 		age--;
-		System.out.println("Decremented age: " + age);
+		if (ageCheck(age)) {
+			System.out.println("Decremented age: " + age);
+		}
+		
+	}
+	
+	public static boolean ageCheck(int age) {
 
+		return ((age >= MIN_AGE) && (age <= MAX_AGE));
 	}
 
 }
