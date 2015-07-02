2b1f40c5c3f676b50294b335fabaa56e3750bd42
Kevin Phair <kevin.phair@gmail.com>
2015-07-02 09:07:59 +0100

    Created an exercise for post increment and decrement

---
+++ OCJA15/src/com/kevinphair/ocja/exercises/PostIncrementDecrementApp.java
@@ -0,0 +1,25 @@
+package com.kevinphair.ocja.exercises;
+
+/*
+ * App to demonstrate post-increment and decrement operations
+ */
+public class PostIncrementDecrementApp {
+	
+	public static void main(String[] args) {
+		
+		int age = 20;
+		
+		// Show the initial value of age
+		System.out.println("Initial value of age: " + age);
+		
+		// Increment and show the new value of age
+		age++;
+		System.out.println("Incremented age: " + age);
+		
+		// Decrement and show the new value of age
+		age--;
+		System.out.println("Decremented age: " + age);
+
+	}
+
+}
