ae25aaf52fa84557b83a0f4064aba76aa398f60d
Kevin Phair <kevin.phair@gmail.com>
2015-07-02 09:07:10 +0100

    Prime number finder app added to repo

---
+++ OCJA15/src/com/kevinphair/ocja/arraytests/PrimeTestApp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.kevinphair.ocja.arraytests;
+
+/**
+ * A java app to find prime numbers using an array (sieve of Eratosthenes)
+ * 
+ * @author Kevin Phair
+ *
+ * We will use an array of booleans to mark each number off the non-primes list
+ */
+public class PrimeTestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		boolean[] nonPrimesFlags = new boolean[20000];
+
+	}
+
+}
