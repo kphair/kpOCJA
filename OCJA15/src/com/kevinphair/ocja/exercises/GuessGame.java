f12b3d6f01cfde361b89a5797e00cc74e3d3a9a8
Kevin Phair <kevin.phair@gmail.com>
2015-07-06 15:37:32 +0100

    Some editing for clarity

--- OCJA15/src/com/kevinphair/ocja/exercises/GuessGame.java
+++ OCJA15/src/com/kevinphair/ocja/exercises/GuessGame.java
@@ -5,29 +5,35 @@ import java.util.Scanner;
 
 public class GuessGame {
 
+	/**
+	 * Guessing game code challenge
+	 *
+	 * Team: Kevin, Jenn, Sam, Mubasher
+	 */
 	public static void main(String[] args) {
 
-		Random rand = new Random();
-		Scanner scan = new Scanner(System.in);
+		boolean debug = true;
 		int randNumber = 0;
 		int userGuess = 0;
 		int guessCount;
-		String userInput = "";
 		int[] haveGuessed = new int[20];
+		String userInput = "";
+		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
 
 		mainLoop:
 			for(;;){
 				randNumber = rand.nextInt(19) + 1;
-				System.out.println(randNumber);
+				if (debug) System.out.println(randNumber);
 				guessCount = 0;
 				
 				// Clear the array which keeps track of the number of times a number was guessed
 				for(int i = 0; i < haveGuessed.length; ++i) {
 					haveGuessed[i] = 0;
 				}
+
 				// Start of the main program loop
 				for(;;){
-					//		System.out.println("Hello World");
 					// Get a number from the user
 					System.out.println("Please enter a number between 1 and 20 inclusive");
 
@@ -85,36 +91,30 @@ public class GuessGame {
 
 						// Check to see if the user's matches the random number the computer picked
 						if(userGuess == randNumber) {
-							System.out.println("Congratulations - The correct number was " + randNumber + ". "
-												+ "It took you " + guessCount + " guesses");
-							break;
-							
+							System.out.println("Congratulations - The correct number was " + randNumber + ".");
+							System.out.println("It took you " + guessCount + " guess" + (guessCount > 0 ? "es." : "."));
+							break;			// Break out of the 'asking for a guess' loop
 						} else {
 							System.out.println("Please guess again");
 						}
 					}
-
-				}// Inner Loop
-
-				//			System.out.println(userInput);
+				} // End of inner Loop
 
 				// Loop to ask if they want to play again
 				System.out.println("Would you like to play again? Y/N");
 				for(;;){
 					userInput = scan.next().toUpperCase();
+					if (debug) System.out.println("You entered " + userInput);
 					if(userInput.equals ("Y")) {
 						break;
 					} else if(userInput.equals("N")) {
-						System.out.println("Goodbye");
+						System.out.println("Thank you for playing. Goodbye!");
 						break mainLoop;
 					} else {
 						System.out.println("Invalid choice - Expecting Y or N...");
 					}
 				}
-
-			}// Top Loop - end of mainLoop
-
+			} // Top Loop - end of mainLoop
 		scan.close();
-		
 	}
 }
