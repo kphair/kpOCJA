c409ea4dd1cf831b3b41f4b84d6c13391a9e13f4
Kevin Phair <kevin.phair@gmail.com>
2015-07-06 09:37:50 +0100

    Added a loop to print name 100 times

--- OCJA15/src/com/kevinphair/ocja/exercises/NameApp.java
+++ OCJA15/src/com/kevinphair/ocja/exercises/NameApp.java
@@ -17,6 +17,13 @@ public class NameApp {
 		System.out.print("  Kevin");
 		System.out.print("  Kevin");
 		System.out.println();
+
+		System.out.println("Print to console 100 times using for() loop:");
+		for (int i = 0; i < 100; ++i) {
+			System.out.print("Kevin  ");
+		}
+		System.out.println();
+		
 		
 	}
 }
