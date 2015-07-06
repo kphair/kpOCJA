6374e940d0c6e069abaaba8f2b3454d39587622f
Kevin Phair <kevin.phair@gmail.com>
2015-07-06 09:36:54 +0100

    Create NameApp to print name 10 times

---
+++ OCJA15/src/com/kevinphair/ocja/exercises/NameApp.java
@@ -0,0 +1,22 @@
+package com.kevinphair.ocja.exercises;
+
+public class NameApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Print to console 10 times with no lopp:");
+		
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.print("  Kevin");
+		System.out.println();
+		
+	}
+}
