6f051bd9bb39d5b569998e42b02e9a1a86c4419d
Kevin Phair <kevin.phair@gmail.com>
2015-07-06 10:04:11 +0100

    Added code to get a name from the user

--- OCJA15/src/com/kevinphair/ocja/exercises/NameApp.java
+++ OCJA15/src/com/kevinphair/ocja/exercises/NameApp.java
@@ -1,5 +1,7 @@
 package com.kevinphair.ocja.exercises;
 
+import java.util.Scanner;
+
 public class NameApp {
 
 	public static void main(String[] args) {
@@ -18,9 +20,21 @@ public class NameApp {
 		System.out.print("  Kevin");
 		System.out.println();
 
+		String userName = "";
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println();
+		System.out.println("Please enter your name:");
+		
+		while (userName == "") {
+			userName = scan.next();
+			System.out.println(userName);
+		}
+		
 		System.out.println("Print to console 100 times using for() loop:");
 		for (int i = 0; i < 100; ++i) {
-			System.out.print(i + ":Kevin  ");
+			System.out.print(i + ":" + userName + "  ");
 		}
 		System.out.println();
 		
