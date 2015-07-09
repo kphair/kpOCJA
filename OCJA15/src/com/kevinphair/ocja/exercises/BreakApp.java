ba5615178ce5f827cf67834fcfd6b759cbaa776f
Kevin Phair <kevin.phair@gmail.com>
2015-07-09 12:02:17 +0100

    Enhanced program to allow one decimal place for searching

--- OCJA15/src/com/kevinphair/ocja/exercises/BreakApp.java
+++ OCJA15/src/com/kevinphair/ocja/exercises/BreakApp.java
@@ -6,25 +6,30 @@ public class BreakApp {
 
 	public static void main(String[] args) {
 
-		int targetNumber = 0;
-		final int MAX_NUM = 100;
+		float targetNumber = 0.0f;
+		final float MAX_NUM = 100.0f;
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("This program will count from 1 to 100.");
-		System.out.println("Please enter a number you would like to stop at.");
+		System.out.println("This program will count from 1 to 100 in steps of 0.1.");
+		System.out.println("Please enter a number you would like to stop at (e.g. 4, 7.2, 42.9, etc.)");
 		do {
-			while (! scan.hasNextInt()) {
+			while (! scan.hasNextFloat()) {
 				scan.next();
 			}
-			targetNumber = scan.nextInt();
+			targetNumber = scan.nextFloat();
 			if ((targetNumber < 1) || (targetNumber > MAX_NUM)) {
-				System.out.println(targetNumber + " is an invalid number. Please try again...");
+				// Check to see if the float has only one digit after the decimal point
+				if (targetNumber * 10 == (int)(targetNumber * 10)) {
+					System.out.println("Seems legit");
+					break;
+				}
+				System.out.println(targetNumber + " is not a valid number. Please try again...");
 				targetNumber = 0;
 			}
 		} while (targetNumber == 0);
 
-		
+		searchloop:
 		for (int i = 1; i <= MAX_NUM; ++i) {
 			
 			System.out.print("Checking " + i + ", ");
@@ -32,11 +37,15 @@ public class BreakApp {
 				System.out.println("\nI did it! I found your number, " + i + "!");
 				break;
 			}
-			for (int j = 1; j <= 9; ++j) {
-				System.out.print(i + "." + j + (j < 9 ? ", " : "\n"));
+			for (float j = 1; j <= 9; ++j) {
+				System.out.println(i + j / 10);
+				if (i + j / 10 == targetNumber) {
+					System.out.println("\nI did it! I found your number, " + targetNumber + "!");
+					break searchloop;
+				}
 			}
 		}
-
+		scan.close();
 	}
 
 }
