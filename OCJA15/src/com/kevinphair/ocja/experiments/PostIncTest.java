cdc019d5fa49e3889f03b04273ca8790d2e08c13
Kevin Phair <kevin.phair@gmail.com>
2015-07-13 11:30:08 +0100

    Experiment with order of operation with post-increment

---
+++ OCJA15/src/com/kevinphair/ocja/experiments/PostIncTest.java
@@ -0,0 +1,13 @@
+package com.kevinphair.ocja.experiments;
+
+public class PostIncTest {
+
+	public static void main(String[] args) {
+
+		int age = 0;
+		
+		System.out.println("Age is " + age++ + " then " + age++);
+		System.out.println("Age is now " + age);
+	}
+
+}
