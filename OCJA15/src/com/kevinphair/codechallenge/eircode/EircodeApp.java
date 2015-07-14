dbb9ed053d2cc293e9e3c95ed706819a89d14f79
Kevin Phair <kevin.phair@gmail.com>
2015-07-14 12:40:59 +0100

    Started coding user menu

--- OCJA15/src/com/kevinphair/codechallenge/eircode/EircodeApp.java
+++ OCJA15/src/com/kevinphair/codechallenge/eircode/EircodeApp.java
@@ -8,8 +8,10 @@ public class EircodeApp {
 
 	public static void main(String[] args) {
 		String userInput;
+		String addressInput;
+		Scanner scan;
 		
-		/*
+		/**
 		 * Set up the eircode and address database
 		 */
 		String [] eircodes = {"D02 Y006", "D04 C932", "D15 XR2R", "D03 RR27", "D24 H510"};
@@ -19,11 +21,34 @@ public class EircodeApp {
 								"26 KINCORA ROAD, Clontarf, Dublin 3.",
 								"Partas, 4A BROOKFIELD ENTERPRISE CENTRE, Dublin 24"};
 		
+
+// 		display "1: Search database by eircode"
+// 		display "2: Search database by address"
+// 		display "3: Search database by area"
+// 		display "4: Add new eircode and address"
+// 		display "5: Dump database to console"
+// 		display "6: Quit"
 		
-		/* create a new Scanner object to get user input
+		System.out.println("Please select from the following options ");
+		System.out.println("1: Search database by eircode");
+		System.out.println("2: Search database by address");
+		System.out.println("3: Search database by area");
+		System.out.println("4: Add new eircode and address");
+		System.out.println("5: Dump database to console");
+		System.out.println("6: Quit");
+		
+		 scan = new Scanner(System.in);
+		 System.out.print("Please enter an option: ");
+		 
+		 while(true) {
+			 userInput = scan.nextLine();
+			 
+		 }
+		/**
+		 *  create a new Scanner object to get user input
 		 * prompt user for input and get input to string
 		 */
-		Scanner scan = new Scanner(System.in);
+		 scan = new Scanner(System.in);
 		System.out.print("Please enter eircode: ");
 		while(true) {
 			userInput = scan.nextLine().toUpperCase();
@@ -33,7 +58,7 @@ public class EircodeApp {
 		}
 		scan.close();							// Scanner no longer needed
 		
-		/*
+		/**
 		 * Scan the eircode array for a matching code using an index
 		 * to iterate over the array
 		 */
@@ -54,7 +79,7 @@ public class EircodeApp {
 		}
 	}
 	
-	/*
+	/**
 	 * Validate the supplied string to see if it's an eircode
 	 * Assumptions:
 	 * If the string is 8 characters, it should have a space as the 4th character
@@ -62,6 +87,7 @@ public class EircodeApp {
 	 * If the string is less than 7 or more than 8 it will considered invalid
 	 * If the string is 8 characters, it will be assumed that the space could be anywhere for a close match
 	 * 
+	 * @param string for checking
 	 * @returns 0 for no match, 1 for a close match, 2 for a perfect match
 	 * 
 	 */
@@ -71,10 +97,10 @@ public class EircodeApp {
 			return 0;											// No match
 		}
 		
-		if (eircode.length() == 8) || eircode.replace(" ", "" != 7) {
+		if ((eircode.length() == 8) || (eircode.replace(" ", "").length() == 7)) {
 			
 		}
-		
+		return 0;
 	}
 
 }
