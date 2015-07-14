8cbf8a6b72a2fd7693fb2aa53249d47e1d710c22
Kevin Phair <kevin.phair@gmail.com>
2015-07-14 09:29:53 +0100

    Populated database.

--- OCJA15/src/com/kevinphair/codechallenge/eircode/EircodeApp.java
+++ OCJA15/src/com/kevinphair/codechallenge/eircode/EircodeApp.java
@@ -8,6 +8,13 @@ public class EircodeApp {
 
 	public static void main(String[] args) {
 		String userInput;
+		String [] eircode = {"D02 Y006", "D04 C932", "D15 XR2R", "D03 RR27", "D24 H510"};
+		String [] addresses = {"5 Merrion Square North, Dublin 2", 
+								"10 Burlington Road, Dublin 4",
+								"Dunsink Observatory, Dunsink Lane, Dublin 15",
+								"26 KINCORA ROAD, Clontarf, Dublin 3.",
+								"Partas, 4A BROOKFIELD ENTERPRISE CENTRE, Dublin 24"};
+		
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Please enter eircode: ");
