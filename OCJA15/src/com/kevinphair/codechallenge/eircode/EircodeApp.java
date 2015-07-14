ac080bb8649c781661efd5706a06e379c7ddfa01
Kevin Phair <kevin.phair@gmail.com>
2015-07-14 09:15:11 +0100

    Created initial app

---
+++ OCJA15/src/com/kevinphair/codechallenge/eircode/EircodeApp.java
@@ -0,0 +1,13 @@
+package com.kevinphair.codechallenge.eircode;
+
+/* See eircodes.txt for specification and sample database
+ */
+public class EircodeApp {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
