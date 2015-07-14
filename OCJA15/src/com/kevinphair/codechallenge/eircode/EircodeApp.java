b8f8c95bf09f22cc7f87e26a04de2f186c6abb02
Kevin Phair <kevin.phair@gmail.com>
2015-07-14 09:23:09 +0100

    Added user input.

--- OCJA15/src/com/kevinphair/codechallenge/eircode/EircodeApp.java
+++ OCJA15/src/com/kevinphair/codechallenge/eircode/EircodeApp.java
@@ -1,12 +1,18 @@
 package com.kevinphair.codechallenge.eircode;
 
+import java.util.Scanner;
+
 /* See eircodes.txt for specification and sample database
  */
 public class EircodeApp {
 
 	public static void main(String[] args) {
-
+		String userInput;
 		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter eircode: ");
+		userInput = scan.next();
+		System.out.println(userInput);
 		
 	}
 
