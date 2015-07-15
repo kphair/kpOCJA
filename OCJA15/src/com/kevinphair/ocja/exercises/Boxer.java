ad0a45e64fe55d4d597c91ea553b0f39e8bf9646
Kevin Phair <kevin.phair@gmail.com>
2015-07-15 15:21:19 +0100

    Boxing example

---
+++ OCJA15/src/com/kevinphair/ocja/exercises/Boxer.java
@@ -0,0 +1,22 @@
+package com.kevinphair.ocja.exercises;
+
+public class Boxer {
+
+	public static void main(String[] args) {
+
+		Boxer box = new Boxer();
+		System.out.println(box.boxingDay(26));
+		
+	}
+
+	boolean boxingDay (Integer tmp) {
+		
+		Boolean isBox = true;
+		Short num = 360;
+		
+		if (isBox) {
+			System.out.println(num);
+		}
+		return !isBox;
+	}
+}
