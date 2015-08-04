be488c346b031ba1eca4929e19ba4a7b671dea41
Kevin Phair <kevin.phair@gmail.com>
2015-08-04 13:52:59 +0100

    Added a Snake class

---
+++ OCJA15/src/nu/xdi/oo_concepts/Snake.java
@@ -0,0 +1,44 @@
+package nu.xdi.oo_concepts;
+
+/**
+ * A class to use in demonstrating object-oriented concepts such as
+ * inheritance, polymorphism, abstraction
+ * 
+ * @author Kevin Phair
+ * Created 4 Aug 2015
+ *
+ */
+public class Snake extends Animal {
+
+	private static final String SOUND = "Hiss!";
+	
+	/**
+	 * Default constructor initialises blank name
+	 */
+	Snake() {
+		this("");
+	}
+	
+	/**
+	 * Constructor to initialise a new object with specified name
+	 * @param name
+	 */
+	Snake (String name) {
+		super (name, Snake.SOUND);
+	}
+	
+	/**
+	 * Snake-specific move method
+	 */
+	public void move () {
+		System.out.println(super.getName() + " is slithering...");
+	}
+	
+	/**
+	 * Snake's sleep method
+	 */
+	public void sleep() {
+		System.out.println(super.getName() + " coils up and goes to sleep");
+	}
+	
+}
