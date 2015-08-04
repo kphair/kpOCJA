62bf488d738200741cd753e18fab41e15c3119e3
Kevin Phair <kevin.phair@gmail.com>
2015-08-04 09:34:24 +0100

    Added an instance of a Cat object

--- OCJA15/src/nu/xdi/oo_concepts/InheritApp.java
+++ OCJA15/src/nu/xdi/oo_concepts/InheritApp.java
@@ -11,9 +11,30 @@ public class InheritApp {
 
 	public static void main (String[] args) {
 		
+		new InheritApp().runProgram();
+	}
+
+	// Perform the required tasks
+	private void runProgram () {
 		Person person = new Person ("Kevin");
-		System.out.println("'person's toString method returns '" + person + "'");
-		person.makeNoise();
+		System.out.println(person + " is present.");
+		
+		Cat cat = new Cat ("Fluff");
+		System.out.println(cat + " is present.");
+		
+		this.talk(person);
+		this.talk(cat);
 		
 	}
+
+	// talk method for Person objects
+	private void talk (Person p) {
+		p.makeNoise();
+	}
+	
+	// talk method for Cat objects
+	private void talk (Cat c) {
+		c.makeNoise();
+	}
+	
 }
