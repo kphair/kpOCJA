83bced148584fab53c850906acfb4615f17db65d
Kevin Phair <kevin.phair@gmail.com>
2015-08-04 13:51:33 +0100

    Added an ArrayList and method to retrieve same for managing all objects

--- OCJA15/src/nu/xdi/oo_concepts/Animal.java
+++ OCJA15/src/nu/xdi/oo_concepts/Animal.java
@@ -1,5 +1,7 @@
 package nu.xdi.oo_concepts;
 
+import java.util.ArrayList;
+
 /**
  * A class to use in demonstrating object-oriented concepts such as
  * inheritance, polymorphism, abstraction
@@ -12,6 +14,7 @@ public abstract class Animal {
 
 	private String name;
 	private String sound;
+	private static ArrayList<Animal> animals;
 	
 	/**
 	 * Default constructor initialises blank name
@@ -27,7 +30,15 @@ public abstract class Animal {
 	Animal (String name, String sound) {
 		this.name = name;
 		this.sound = sound;
+		if (Animal.animals == null) Animal.animals = new ArrayList<Animal>();
+		Animal.animals.add(this);
 	}
+	
+	/**
+	 * Object goes to sleep but don't define it here,
+	 * make it abstract so subclasses must take care of it
+	 */
+	abstract public void sleep ();
 	
 	/**
 	 * This object makes a noise
@@ -43,7 +54,27 @@ public abstract class Animal {
 		return this.name;
 	}
 	
+	/**
+	 * General move animal method
+	 * @return
+	 */
+	public void move() {
+		System.out.println(this.name + " is moving...");
+	}
+	
+	/**
+	 * Get the name of this animal
+	 * @return
+	 */
 	public String getName() {
 		return this.name;
+	}
+	
+	/**
+	 * Get the ArrayList of all Animal objects
+	 * 
+	 */
+	public static ArrayList<Animal> getArrayList () {
+		return Animal.animals;
 	}
 }
