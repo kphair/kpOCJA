6368003b18d103fe769335ceee4a1d46c0b6e10e
Kevin Phair <scorpio@xdi.nu>
2015-08-12 15:34:55 +0100

    PPSN generator added

---
+++ OCJA15/src/nu/xdi/code_challenges/MakePPSN.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class MakePPSN {
+
+	public static void main (String[] args) {
+
+		System.out.println (make("6986035"));
+
+		while (true) {
+			System.out.println (make(String.format("%d", new Random().nextInt(9000000) + 1000000)));
+		}	
+
+	}
+
+	public static String make(String ppsn) {
+
+	    int w = 8;
+	    int sum = 0;
+
+		for (int i = 0; i < 7; ++i) {
+			sum += Integer.parseInt(ppsn.charAt(i) + "") * w--; 
+		}
+
+		return (ppsn + (char)('A' + (sum - 1) % 23));
+	}
+
+}
